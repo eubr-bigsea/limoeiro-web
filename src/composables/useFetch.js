22a0bcc3d63b8213d1951edf5af4d43731955d3d
MarcoTuMD <marcotu.md@gmail.com>
2025-04-24 13:14:47 -0300

    implemented apim

--- src/composables/useFetch.js
+++ src/composables/useFetch.js
@@ -1,20 +1,27 @@
 import { ref } from 'vue'
 import { fetchResource } from '@/services/api.js'
+import { useAuthToken } from '@/composables/useAuthToken.js'  
 
-export function useFetch(endpoint, options) {
+export function useFetch(endpoint, options = {}) {
   const data = ref(null)
   const error = ref(null)
   const isLoading = ref(false)
 
-  const defaults = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
   const fetchData = async () => {
     isLoading.value = true
+    const token = await useAuthToken()
+
+    const headers = {
+      'Content-Type': 'application/json',
+      ...(options.headers || {}),
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    }
+
     try {
-      data.value = await fetchResource(endpoint, { ...defaults, ...options })
+      data.value = await fetchResource(endpoint, {
+        ...options,
+        headers,
+      })
     } catch (err) {
       error.value = err
     } finally {
